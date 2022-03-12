51d49c58c20e4d2e4fba76a19918d886f0894a13
Noah <35441938+noahfarber@users.noreply.github.com>
2022-03-12 15:07:28 -0800

    Adds editor script for quickly creating state classes

---
+++ Scripts/Editor/Editor.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+using System.Text;
+using System;
+using Framework;
+
+public class Editor : MonoBehaviour
+{
+    [MenuItem("Assets/Create/CreateStateClass %#d", false, 0)]
+    private static void CreateStateClass()
+    {
+        GameObject selected = Selection.activeObject as GameObject;
+        if (selected == null || selected.name.Length == 0)
+         {
+            Debug.LogError("A GameObject must be selected to provide the state class name.");
+            return;
+        }
+
+        // remove whitespace and minus
+        string fileName = selected.name.Replace(" ", "_").Replace("-", "_");
+
+        string createPath = "Assets/" + fileName + ".cs";
+        string copyFromPath = "Assets/Framework/Scripts/Editor/ExampleState.cs";
+
+        if (File.Exists(createPath) == false)
+        {
+            using (StreamWriter outfile = new StreamWriter(createPath))
+            {
+                if(File.Exists(copyFromPath) != false)
+                {
+                    using (StreamReader reader = new StreamReader(copyFromPath))
+                    {
+                        string contents = reader.ReadToEnd();
+                        contents = contents.Replace("ExampleState", fileName);
+                        outfile.WriteLine(contents);
+                        Debug.Log("Creating State Class: " + fileName);
+                    }
+                }
+            }
+        }
+
+        AssetDatabase.Refresh();
+    }
+}
